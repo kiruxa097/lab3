e7b424b742b520eb2c653c679700bf94f422e6d5
Kirill <mordvinkin2740@gmail.com>
2026-05-17 13:43:03 +0300

    Пофиксил баг с повторным элементом в поиске макс. элемента строки

--- matrix.cpp
+++ matrix.cpp
@@ -25,12 +25,19 @@ class Matrix : public vector<vector<T>> {
 
             // Поиск максимального по модулю значения в строке - выбор ведущего элемента
             for (int i = 0; i < line; i++){
-                T max_abs_val = (*this)[i][0];
+
+                T max_abs_val = 0;
                 int t_p = 0;
                 for (int j = 0; j < col; j++){
                     if (abs((*this)[i][j]) > max_abs_val) {
-                        max_abs_val = abs((*this)[i][j]);
-                        t_p = j;
+                        bool flag = true;
+                        for (int q = 0; q < i; q++) {
+                            if (arr_num[q] == j) { flag = false; break; }
+                        }
+                        if (flag) {
+                            max_abs_val = abs((*this)[i][j]);
+                            t_p = j;
+                        }
                     }
                 }
                 max_values[i] = max_abs_val;
@@ -42,10 +49,21 @@ class Matrix : public vector<vector<T>> {
 
             // Зануляем не ведущие элементы в столбцах
             for (int i = 0; i < line; i++) {
-                for (int j = 0; j < line-1; j++){
-                    
+                T t1 = max_values[i]; // умножаем это число на строку, в которой будем занулять элемент 
+                for (int j = i + 1; j < line; j++){
+                    T t2 = (*this)[j][arr_num[i]]; // умножаем это число на строку, которую будем вычитать 
+                    for (int u = 0; u < col; u++) {
+                        (*this)[j][u] = (*this)[j][u]*t1 - (*this)[i][u]*t2;
+                    }
+                    max_values[j] = (*this)[j][arr_num[j]];
+                }
+                for (int j = i - 1; j >= 0; j--){
+                    T t2 = (*this)[j][arr_num[i]]; // умножаем это число на строку, которую будем вычитать 
+                    for (int u = 0; u < col; u++) {
+                        (*this)[j][u] = (*this)[j][u]*t1 - (*this)[i][u]*t2;
+                    }
+                    max_values[j] = (*this)[j][arr_num[j]];
                 }
             }
-
         }
 };
