8b1bb79f3694249d980608111cb5496c94769550
kiruxa097 <mordvinkin2740@gmail.com>
2026-05-17 18:02:18 +0300

    main файл с реализацией

--- main.cpp
+++ main.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include "matrix.cpp"
 
 using namespace std;
 
 int main()
 {
-    Matrix<float> m(3,4);
-    m[0][1] = 1;
-    cout << m[0][1];
+    cout << "Last column in vector is vector ans" << endl;
+    int row, column;
+    cout << "Input row matrix: ";
+    cin >> row;
+    cout << "Input column matrix: ";
+    cin >> column;
+    cout << endl;
+    Matrix<float> m(row, column);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            cout << "Input [" << i << "]" << "[" << j << "]" << " value: ";
+            cin >> m[i][j];
+        }
+    }
+    m.print();
+    m.gaus();
     return 0;
 }
